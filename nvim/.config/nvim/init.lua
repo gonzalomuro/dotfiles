94fa1593f5cc475cede71453e1e87ae4c27a22ce
Gonzalo Muro <gmurop@gmail.com>
2026-07-14 14:30:39 -0400

    Now obsidian keymaps and obsidian nvim config changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -149,7 +149,40 @@ lazy.setup({
           name = "personal",
           path = "~/personal-vault",
         }
-      }
+      },
+      notes_subdir = "notes",
+      daily_notes = {
+        folder = "notes/daily"
+      },
+      mapping = {
+        -- Toggle check-boxes.
+        ["<leader>oc"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+      new_notes_location = "notes_subdir",
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+      end,
     }
   },
 })
--- nvim/.config/nvim/keys.lua
+++ nvim/.config/nvim/keys.lua
@@ -6,8 +6,11 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function(ev)
     local opts = { buffer = ev.buf, silent = true }
     vim.keymap.set("n", "gd", "<cmd>Obsidian follow_link<CR>", opts)
+    vim.keymap.set("n", "<leader>on", "<cmd>Obsidian new<CR>", opts)
     vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian open<CR>", opts)
     vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian today<CR>", opts)
+    vim.keymap.set("n", "<leader>oy", "<cmd>Obsidian yesterday<CR>", opts)
+    vim.keymap.set("n", "<leader>of", "<cmd>Obsidian search<CR>", opts)
     vim.keymap.set("n", "<CR>", "<cmd>Obsidian toggle_checkbox<CR>", opts)
   end,
 })
@@ -19,6 +22,7 @@ vim.keymap.set({ 'n', 'x', 'o' }, '<leader>h', '^')
 vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', 'g_')
 vim.keymap.set({ 'n', 'x', 'o' }, '<Tab>', '<cmd>bn<cr>')
 vim.keymap.set({ 'n', 'x', 'o' }, '<S-Tab>', '<cmd>bp<cr>')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>v', '<cmd>vs<cr>')
 
 -- fzf-lua
 vim.keymap.set('n', '<leader>b', "<cmd>lua require('fzf-lua').buffers()<cr>", { silent = true })
