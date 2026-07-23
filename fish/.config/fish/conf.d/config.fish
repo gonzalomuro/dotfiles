989a3d104bd98195a2c6e5af539a2f539eb03f84
Gonzalo Muro <gmurop@gmail.com>
2026-07-23 14:23:19 -0400

    Enable copy when yanking from ssh session

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr -a -- gs git status
+    abbr -a -- home nvim '$VAULT_HOME/0-home.md'
 		set -g fish_key_bindings fish_vi_key_bindings
 		eval (/opt/homebrew/bin/brew shellenv)
 
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,6 +43,18 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 5
 vim.opt.clipboard = "unnamedplus"
 
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 -- plugins
 local lazy = {}
 function lazy.install(path)
